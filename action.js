5ad560e7ac4cf08458b96396e26b3817a481edd9
di <di@gmail.com>
2023-11-10 23:24:08 +0700

    update h3 index

--- action.js
+++ action.js
@@ -1,1 +1,11 @@
-alert( "I'm JavaScript!" )
+
+for (let i = 0; i < 3; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, 1000);
+}
+
+let sum = (a, b) => 
+     a + b;
+let a = sum(2, 3)
+console.log(a);
