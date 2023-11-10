49323dc7e725b6af5797bf843e0f98573081fcfa
di <di@gmail.com>
2023-11-10 23:29:12 +0700

    update cong

--- action.js
+++ action.js
@@ -1,4 +1,3 @@
-
 for (let i = 0; i < 3; i++) {
   setTimeout(() => {
     console.log(i);
@@ -7,5 +6,5 @@ for (let i = 0; i < 3; i++) {
 
 let sum = (a, b) => 
      a + b;
-let a = sum(2, 3)
-console.log(a);
+let cong = sum(2, 3)
+console.log(cong);
